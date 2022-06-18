062d79a0581d58746fcda858ff36c9844ab61f1a
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-06-18 17:21:12 -0500

    completed README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# timed-code-quiz
+# Timed code quiz
+
+https://caitlinparsons25.github.io/timed-code-quiz/
+
+## Aims of the project
+1. When the user clicks the "Start quiz" button, the quiz and the 60-second timer start
+2. When the user chooses an answer, text will appear letting them know whether they chose the correct or incorrect answer
+3. An incorrect answer deducts 5 seconds from the timer
+4. When the user clicks the "Next question" button, the next question appears
+
+### 1. When the user clicks the "Start quiz" button, the quiz and the 60-second timer start
+When the user first navigates to the app, they are presented with an explanation of the quiz and a button they can click to start the quiz.
+![picture](./assets/readme/start-screen.png)
+When they click on the "Start quiz" button, the quiz and timer both begin.
+![picture](./assets/readme/quiz-start.png)
+
+### 2. When the user chooses an answer, text will appear letting them know whether they chose the correct or incorrect answer
+If the user chooses the correct answer, text will appear in green letting them know they have chosen the correct answer.
+![picture](./assets/readme/correct.png)
+If the user chooses the incorrect answer, text will appear in red letting them know they have chosen an incorrect answer.
+![picture](./assets/readme/incorrect.png)
+
+### 3. An incorrect answer deducts 5 seconds from the timer
+When the user chooses an incorrect answer, 5 seconds are dedcuted from the timer; this is done using `time -=5` in the code for the function that checks the answer.
+
+### 4. When the user clicks the "Next question" button, the next question appears
+Upon clicking the "Next question" button, the data from question 1 is replaced by the data for question 2.
+![picture](./assets/readme/next-question.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,92 @@
 var startButton = document.querySelector("#start-button");
 
+var time = 60;
+let questionNumber = 0;
 
-var startQuiz = function() {
-    function startTimer (duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
+let questions = [{
+    question: "question 1",
+    options: ["option1", "option2", "option3"],
+    answer: "option1"
+}, {
+    question: "question 2",
+    options: ["option1", "option2", "option3"],
+    answer: "option2"
+}]
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+var startTimer = ()=> {
+    setInterval(function () {
+        var timer = document.getElementById("timer");
+        if (time >= 0) {
+            timer.innerHTML = "Time remaining: " + time;
+            time = time - 1;            
+    }}, 1000)
+};
 
-            display.textContent = minutes + ":" + seconds;
+let div = document.createElement("div");
+div.setAttribute("id", "question-div");
 
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
+function generateOptions(){
+    let h1 = document.createElement("h1")
+    h1.innerHTML = questions[questionNumber].question
+    div.appendChild(h1);
+    let unorderedList = document.createElement("ul")
+    for (let i = 0; i < questions[questionNumber].options.length; i++){
+        let listItem = document.createElement("li");
+        listItem.setAttribute("data-answer", questions[questionNumber].answer);
+        listItem.innerHTML = questions[questionNumber].options[i];
+        listItem.addEventListener("click", checkAnswer);
+        unorderedList.appendChild(listItem);
     }
+    div.appendChild(unorderedList);
+    return div;
+};
+
+let span = document.createElement("span");
+let buttonEl = document.createElement("button");
+
+
+function checkAnswer(event){
+    event.preventDefault();
+   let selectedOption = event.target.innerHTML;
+   let correctAnswer = event.target.dataset.answer;
+   if (selectedOption === correctAnswer){
+      span.innerHTML = "Correct answer";
+      span.setAttribute("id", "correct-answer");
+      buttonEl.innerHTML = "Next question";
+      buttonEl.addEventListener("click", nextQuestion);
+        document.getElementById("question-div").append(span);
+        document.getElementById("question-div").disable = true;
+   } else {
+    span.innerHTML = "Incorrect answer";
+    span.setAttribute("id", "incorrect-answer");
+    document.getElementById("question-div").append(span);
+    time -= 5;
+    buttonEl.innerHTML = "Next question";
+    buttonEl.addEventListener("click", nextQuestion);
+    document.getElementById("question-div").disable = true;
+   }
+   document.getElementById("question-div").append(buttonEl);
+};
+
+function nextQuestion(){
+    questionNumber += 1;
+    document.getElementById("question-div").innerHTML = "";
+
+    generateHTML();
+}
+
+function generateHTML(){
     
-    var countDown = function () {
-        var twoMinutes = 60 * 2,
-            display = document.querySelector("#time");
-        startTimer(twoMinutes, display);
-    }
-
-    countDown();
-
+    let generatedDiv = generateOptions();
+    document.getElementById("show-options").append(generatedDiv);
 
 };
+
+var startQuiz = function () {
+    document.getElementById("introduction").setAttribute("class", "toggledisplay");
+    startTimer();
+    generateHTML()
+};
+
 
 startButton.addEventListener("click", startQuiz);
